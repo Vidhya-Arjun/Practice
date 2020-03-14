ff03a37353ffa254c965a4e343a4f20f71ed9ce9
Arjun Nakkeeran <arjun.nakkeeran@live.in>
2020-03-14 22:03:13 +0100

    Use format for print value

--- Colorconsole.java
+++ Colorconsole.java
@@ -1,21 +1,24 @@
 class ConstructorEx
 {
-     double principle_Amount;
-     int Number_of_years;
-     float rate_of_interest;
+    double principle_Amount;
+    int Number_of_years;
+    float rate_of_interest;
    
-     ConstructorEx()
+    ConstructorEx()
     {
          principle_Amount = 10000.00;
          Number_of_years = 2;
          rate_of_interest = 10;
     }
+
     void calculate()
     {
         double Simple_Interest = (principle_Amount * Number_of_years * rate_of_interest)/100 ;
-        System.out.println("The Simpile Interest Amount is: " +Simple_Interest );
+        System.out.println("The Simpile Interest Amount is: " + Simple_Interest );
+        System.out.printf("The SI %d", Simple_Interest);
     }
-   public static void main(String[] args)
+
+    public static void main(String[] args)
     {
         ConstructorEx ob = new ConstructorEx();
         ob.calculate(); 
